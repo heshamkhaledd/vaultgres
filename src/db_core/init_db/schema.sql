426fcca6d049bbcef06ab47d3bcd52f6659b2236
Hesham Khaled <54694393+heshamkhaledd@users.noreply.github.com>
2026-02-14 02:31:17 +0200

    Add primary key to users table id column

--- src/db_core/init_db/schema.sql
+++ src/db_core/init_db/schema.sql
@@ -1,6 +1,6 @@
 -- USERS TABLE
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
